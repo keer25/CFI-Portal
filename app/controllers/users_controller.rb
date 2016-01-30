6d68399633aeb899497017898d559635a4300077
KeerthanaS <keerukeerthana8@gmail.com>
2016-01-30 12:11:36 +0530

    Added Signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create 
+  	@user = User.new(user_params)
+  	if @user.save 
+  		#Redirect to Configuring account
+  		logger.info( "User Created in database")
+  	else 
+  		render 'new'
+  	end
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
-  get 'users/new'
+  get 'signup' => 'users#new'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
