a9383bf1679c01485628440224f0b45782faa4eb
KeerthanaS <keerukeerthana8@gmail.com>
2016-01-30 10:05:49 +0530

    Added Basic User Model

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
 
 source 'https://rubygems.org'
+gem 'bcrypt', '~> 3.1.7'
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# Use Unicorn as the app server
+# gem 'unicorn'
 
 gem 'rails',        '4.2.2'
 gem 'bootstrap-sass', '3.3.6'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
+  get 'users/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
