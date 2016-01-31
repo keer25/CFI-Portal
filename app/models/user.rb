c8e8170e42916f02fd5fec0617918693ca35945c
KeerthanaS <keerukeerthana8@gmail.com>
2016-01-31 21:14:33 +0530

    Added client side validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, 
 	          uniqueness: {  case_sensitive: false }
-	validates :password, presence: true, length: { minimum: 6}
+	validates :password, presence: true, length: { minimum: 6, maximum: 50}
 	has_secure_password
 	belongs_to :club
 	has_many :bills
